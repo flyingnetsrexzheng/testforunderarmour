b76c39d42cb509fbda41f7092b80a02a4e05a2a0
rexzheng <admin@example.com>
2023-02-03 11:47:30 +0800

    CMPaws1

--- flaskzheng.py
+++ flaskzheng.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/v1')
 def hello_word():
-    return "SH There is Under Armour in Flyingnets Alibaba Cloud-2-3\n"
+    return "SH There is Under Armour in Flyingnets AWS-2-3\n"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
