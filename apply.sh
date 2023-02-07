7ce2509d35124d58322e7d51a19d1fd800eacd7e
rexzheng <rex_zheng@flyingnets.com>
2023-02-08 02:08:34 +0800

    AWSNewCMP6

--- apply.sh
+++ apply.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-cd /home/ec2-user/actions-runner/_work/testforunderarmour/testforunderarmour/ && kubectl apply -f UnderArmourtest.yaml
+kubectl apply -f /home/ec2-user/actions-runner/_work/testforunderarmour/testforunderarmour/UnderArmourtest.yaml
 
