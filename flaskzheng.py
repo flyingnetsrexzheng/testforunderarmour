58072d0d3cdf95213311e7b90f6a8df2e084d66d
rexzheng <rex_zheng@flyingnets.com>
2023-02-08 10:15:56 +0800

    AWSNewCMP10

--- flaskzheng.py
+++ flaskzheng.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/v1')
 def hello_word():
-    return "SH There is Under Armour in Flyingnets AWS-2-8 NewCMP-remote\n"
+    return "SH There is Under Armour in Flyingnets AWS-2-8-10:15 NewCMP-remote\n"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
